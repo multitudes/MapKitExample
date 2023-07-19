fb04e1c2a37638cfe454dfd020ce6404c7d95c55
Laurent Brusa <wrmultitudes@gmail.com>
2023-07-19 15:20:48 +0200

    created the Xcode project

---
+++ MapKitExample/MapKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitExampleApp.swift
+//  MapKitExample
+//
+//  Created by Laurent B on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
