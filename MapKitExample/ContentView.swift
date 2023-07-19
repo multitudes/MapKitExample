5e01cee03a7bac7f9dcada68cdd7c9a897577967
Laurent Brusa <wrmultitudes@gmail.com>
2023-07-19 15:32:32 +0200

    add code

--- MapKitExample/ContentView.swift
+++ MapKitExample/ContentView.swift
@@ -8,9 +8,13 @@
 import MapKit
 import SwiftUI
 
+extension CLLocationCoordinate2D {
+    static let parking = CLLocationCoordinate2D(latitude: 42.354528, longitude: -71.068369)
+}
+
 struct ContentView: View {
     var body: some View {
-       Map()
+        Map()
     }
 }
 
